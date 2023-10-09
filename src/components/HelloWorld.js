1da9d2040567a7a5b40471d72a7ea23a31410c77
João ("Jonny") Vitor <118491763+Joao-Jonny-Vitor@users.noreply.github.com>
2023-10-09 20:26:08 -0300

    Add files via upload

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+import Frase from "./Frase"
+
+function HelloWorld(){
+
+    return (
+        <div>
+            
+            <hi>Meu primeiro componente</hi>
+            <Frase/>
+            
+        </div>
+    )
+}
+
+export default HelloWorld
